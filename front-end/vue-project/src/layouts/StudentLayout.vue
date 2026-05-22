e3dad9756b096f6b7ddd3148bdee811c016ec02a
VuDangHuong <145440898+VuDangHuong@users.noreply.github.com>
2026-05-22 09:49:06 +0700

    add SVG logout with Students

--- front-end/vue-project/src/layouts/StudentLayout.vue
+++ front-end/vue-project/src/layouts/StudentLayout.vue
@@ -87,14 +87,7 @@
           @click="logout"
           class="nav-link w-full text-red-500 hover:bg-red-50 hover:text-red-600"
         >
-          <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-            />
-          </svg>
+          <SvgIcon name="logout" class="w-4 h-4" />
           <span>Đăng xuất</span>
         </button>
       </div>
