89a8d72d3da860678509e3ce4230a0f79fcaa2be
VuDangHuong <145440898+VuDangHuong@users.noreply.github.com>
2026-06-28 14:10:18 +0700

    fix url cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://datn-ten-alpha.vercel.app/login'],
+    'allowed_origins' => ['https://datn-ten-alpha.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
