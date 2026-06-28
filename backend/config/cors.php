f77e0b97e8e7598d33897ec0c8d26980a0afa4bd
VuDangHuong <145440898+VuDangHuong@users.noreply.github.com>
2026-06-28 14:09:21 +0700

    update url cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://datn-ten-alpha.vercel.app/login'],
 
     'allowed_origins_patterns' => [],
 
