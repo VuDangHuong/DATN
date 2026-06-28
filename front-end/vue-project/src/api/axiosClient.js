f2fbad5b87c8aa62804812ef45ff5fdd63714bca
VuDangHuong <145440898+VuDangHuong@users.noreply.github.com>
2026-06-28 14:15:06 +0700

    fix axiousClient.js baseURL

--- front-end/vue-project/src/api/axiosClient.js
+++ front-end/vue-project/src/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8080/api',
+  baseURL: 'https://datn-production-d208.up.railway.app/api',
   headers: {
     // 'Content-Type': 'application/json',
     Accept: 'application/json',
