4b02742dbcee9d33649e4dd669405fedd52d90cd
VuDangHuong <145440898+VuDangHuong@users.noreply.github.com>
2026-06-29 11:53:10 +0700

    add url for docker

--- front-end/vue-project/src/api/axiosClient.js
+++ front-end/vue-project/src/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'https://datn-production-d208.up.railway.app/api',
+  baseURL: 'https://datn-production-d208.up.railway.app/api' || 'http://127.0.0.1:8080/api',
   headers: {
     // 'Content-Type': 'application/json',
     Accept: 'application/json',
